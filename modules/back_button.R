df9b6c6f4cb27da603cbafe4f35e9b3a69b1712f
Danielle <danielle@kendallspace.net>
2025-08-12 14:51:24 -0700

    simplify and use global styling

--- modules/back_button.R
+++ modules/back_button.R
@@ -1,28 +1,14 @@
 # nolint start
 
 create_back_button <- function() {
-  tagList(
-    # Back button to return to dashboard
-    tags$a(
-      href = "?",
-      tags$div(id = "backButtonArrow", class = "arrow-container")
-    ),
-    tags$style(HTML("
-      .arrow-container {
-        width: 30px;
-        height: 30px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
-      }
-
-      .arrow-container::before {
-        content: '\\2190'; /* Unicode for left arrow */
-        font-size: 35px;
-        color: #2C3E50;
-      }
-    "))
+  div(
+    style = "margin-bottom: 20px;",
+    actionButton(
+      "back_to_dashboard",
+      "← Back to Dashboard",
+      onclick = "filterManagement.backToDashboard();",
+      class = "btn btn-primary btn-lg"
+    )
   )
 }
 
