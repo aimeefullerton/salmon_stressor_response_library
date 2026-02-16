7cf0eecd8367e22396b77674afe2a787430319b1
Danielle <danielle@kendallspace.net>
2026-02-16 15:33:39 -0800

    fix(issue-11): correct the csv template

--- modules/csv_template.R
+++ modules/csv_template.R
@@ -4,12 +4,13 @@ get_csv_template <- function() {
     curve.id = rep("c1", 5),
     stressor.label = rep("temperature", 5),
     stressor.x = c(10, 15, 20, 25, 30),
-    stressor.units = rep("degC", 5),
+    units.x = rep("degC", 5),
     response.label = rep("survival", 5),
-    response.y = c(0.95, 0.85, 0.70, 0.50, 0.20),
-    response.units = rep("proportion", 5),
-    lower.limit = c(0.90, 0.80, 0.65, 0.45, 0.15),
-    upper.limit = c(1.00, 0.90, 0.75, 0.55, 0.25),
+    response.y = c(0.95, 0.85, 0.70, 0.50, 0.30),
+    units.y = rep("proportion", 5),
+    stressor.value = rep("constant", 5),
+    lower.limit = c(0.90, 0.80, 0.65, 0.45, 0.25),
+    upper.limit = c(1.00, 0.90, 0.75, 0.55, 0.35),
     sd = rep(0.05, 5),
     stringsAsFactors = FALSE
   )
