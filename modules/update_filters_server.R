a081169940f6816ade17932e56292c8327bf6539
Paxton Calhoun <174494779+Paxtonc07@users.noreply.github.com>
2026-05-04 13:19:56 -0700

    Update update_filters_server.R

--- modules/update_filters_server.R
+++ modules/update_filters_server.R
@@ -1,27 +1,29 @@
 # nolint start
 update_filters_server <- function(input, output, session, data, db) {
 
-  # 1. Standard config (Keep as is)
+  # 1. Standard config
   array_cols <- c(
     "species_common_name", "latin_name", "life_stages", "activity", "season",
     "location_country", "location_state_province",
     "location_watershed_lab", "location_river_creek", "function_derivation"
   )
 
-  filter_specs <- list(
-    list(id = "stressor", col = "stressor_name"),
-    list(id = "stressor_metric", col = "specific_stressor_metric"),
-    list(id = "species", col = "species_common_name"),
-    list(id = "life_stage", col = "life_stages"),
-    list(id = "activity", col = "activity"),
-    list(id = "latin_name", col = "latin_name"),
-    list(id = "article_type", col = "article_type"),
-    list(id = "location_country", col = "location_country"),
-    list(id = "location_state_province", col = "location_state_province"),
-    list(id = "location_watershed_lab", col = "location_watershed_lab"),
-    list(id = "location_river_creek", col = "location_river_creek"),
-    list(id = "broad_stressor_name", col = "broad_stressor_name")
-  )
+filter_specs <- list(
+  list(id = "stressor", col = "stressor_name"),
+  list(id = "stressor_metric", col = "specific_stressor_metric"),
+  list(id = "species", col = "species_common_name"),
+  list(id = "life_stage", col = "life_stages"),
+  list(id = "activity", col = "activity"),
+  list(id = "latin_name", col = "latin_name"),
+  list(id = "article_type", col = "article_type"),
+  list(id = "location_country", col = "location_country"),
+  list(id = "location_state_province", col = "location_state_province"),
+  list(id = "location_watershed_lab", col = "location_watershed_lab"),
+  list(id = "location_river_creek", col = "location_river_creek"),
+  list(id = "broad_stressor_name", col = "broad_stressor_name"),
+  list(id = "season", col = "season"),              # Added
+  list(id = "function_derivation", col = "function_derivation") # Added
+)
 
   # ... (Keep apply_filter and get_dynamic_vals helpers exactly as they are) ...
   apply_filter <- function(df, vals, col) {
@@ -51,45 +53,39 @@ update_filters_server <- function(input, output, session, data, db) {
     return(sort(vals))
   }
 
-  # 2. THE SECRET SAUCE: Debounced Input Watcher
-  # This collects all filter inputs into one bucket and waits 800ms 
-  # after the last click before telling the server to update choices.
   to_watch <- reactive({
     lapply(filter_specs, function(s) input[[s$id]])
   })
   
-  # Wait for 800ms of "silence" before triggering cascading updates
-  debounced_watch <- to_watch %>% debounce(800)
+debounced_watch <- to_watch %>% debounce(300)
 
-  # 3. Automatic Cascading Observer
-  observeEvent(debounced_watch(), {
-    req(input$main_navbar == "dashboard")
-    
-    # Get the static values currently in the inputs
-    current_selections <- isolate(to_watch())
-    names(current_selections) <- sapply(filter_specs, function(s) s$id)
+observeEvent(debounced_watch(), {
+  req(input$main_navbar == "dashboard")
+  
+  current_selections <- isolate(to_watch())
+  names(current_selections) <- sapply(filter_specs, function(s) s$id)
 
-    for (spec in filter_specs) {
-      # Calculate subset of valid data based on OTHER selections
-      df_sub <- data
-      for (other_spec in filter_specs) {
-        if (other_spec$id != spec$id) {
-          df_sub <- apply_filter(df_sub, current_selections[[other_spec$id]], other_spec$col)
-        }
+  for (spec in filter_specs) {
+    # Calculate subset of valid data based on ALL OTHER selections
+    df_sub <- data
+    for (other_spec in filter_specs) {
+      if (other_spec$id != spec$id) {
+        df_sub <- apply_filter(df_sub, current_selections[[other_spec$id]], other_spec$col)
       }
-
-      valid_choices <- get_dynamic_vals(df_sub, spec$col)
-      
-      # Stop the loop: only update if the choices are actually different
-      # We use 'freeze' to keep the UI from jumping
-      freezeReactiveValue(input, spec$id)
-      
-      updateSelectizeInput(session, spec$id,
-        choices  = valid_choices,
-        selected = current_selections[[spec$id]],
-        server   = TRUE 
-      )
     }
-  }, ignoreInit = FALSE)
+
+    valid_choices <- get_dynamic_vals(df_sub, spec$col)
+    
+    # CRITICAL: Switch server to FALSE to stop focus-loss glitches
+    # Using freezeReactiveValue prevents the change from triggering itself instantly
+    freezeReactiveValue(input, spec$id)
+    
+    updateSelectizeInput(session, spec$id,
+      choices  = valid_choices,
+      selected = current_selections[[spec$id]],
+      server   = FALSE 
+    )
+  }
+}, ignoreInit = FALSE)
 }
 # nolint end
