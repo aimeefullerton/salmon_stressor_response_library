3da909f6e1f29276ff694f1c2e653293f1f14414
Paxton Calhoun <174494779+Paxtonc07@users.noreply.github.com>
2026-05-04 22:40:53 -0700

    Update reset_filters.R

--- modules/reset_filters.R
+++ modules/reset_filters.R
@@ -1,13 +1,20 @@
 # nolint start
 reset_filters_server <- function(input, session) {
   observeEvent(input$reset_filters, {
-    filters <- c("stressor", "stressor_metric", "species", "life_stage", "activity", 
-                 "latin_name", "article_type", "location_country", "location_state_province", 
-                 "location_watershed_lab", "location_river_creek", "broad_stressor_name")
-    
-    for (f in filters) {
-      updateSelectizeInput(session, f, selected = character(0))
-    }
+    updateTextInput(session, "search", value = "")
+
+    updatePickerInput(session, "stressor", selected = character(0))
+    updatePickerInput(session, "stressor_metric", selected = character(0))
+    updatePickerInput(session, "species", selected = character(0))
+    updatePickerInput(session, "life_stage", selected = character(0))
+    updatePickerInput(session, "activity", selected = character(0))
+    updatePickerInput(session, "latin_name", selected = character(0))
+    updatePickerInput(session, "article_type", selected = character(0))
+    updatePickerInput(session, "location_country", selected = character(0))
+    updatePickerInput(session, "location_state_province", selected = character(0))
+    updatePickerInput(session, "location_watershed_lab", selected = character(0))
+    updatePickerInput(session, "location_river_creek", selected = character(0))
+    updatePickerInput(session, "broad_stressor_name", selected = character(0))
   })
 }
 # nolint end
